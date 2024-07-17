34330ea08dac0daf19f6d9c585b2b499090dc660
ByteBelle-21 <jrg814@usask.ca>
2024-07-17 10:43:46 -0400

    Implemented code for login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -16,19 +16,39 @@ const isValid = (username)=>{
 }
 
 const authenticatedUser = (username,password)=>{ 
-    const User = users.filter((user)=>(
-        user.username===username && user.password === password
-    ))
-    if(User.length>0){
-        return true;
+    if(isValid(username)){
+        const User = users.filter((user)=>(
+            user.username===username && user.password === password
+        ))
+        if(User.length>0){
+            return true;
+        }
     }
+ 
     return false;
 }
 
+
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const Username = req.body.username;
+    const Password = req.body.password;
+    if(!Username || !Password){
+        return res.status(404).json({message: "Username or password is empty string"});
+    }else{
+        if(authenticatedUser(Username, Password)){
+            let accessToken  = jwt.sign({
+                data: Password
+            }, 'access', { expiresIn: 60 * 60 });
+
+            req.session.authorization = {
+                accessToken, Username
+            }
+            return res.status(200).send("User successfully logged in");
+        } else {
+            return res.status(208).json({ message: "Invalid Login. Check username and password" });
+        }
+    }
 });
 
 // Add a book review
