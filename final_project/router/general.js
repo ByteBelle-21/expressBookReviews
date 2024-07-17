791ba6c95aab4abda803d1b42279356fd1a37111
ByteBelle-21 <jrg814@usask.ca>
2024-07-16 22:31:15 -0400

    Implemented the code for getting the list of books available in the shop

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,11 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let bookList = [];
+    Object.values(books).map((element)=>(
+    bookList.push(element["title"])
+  ))
+  return res.send(JSON.stringify(bookList));
 });
 
 // Get book details based on ISBN
